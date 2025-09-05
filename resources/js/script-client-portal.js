8f689427adbd720d3bf4d3e4d1d46245b832aee9
Jenny Porada <jennyingles@gmail.com>
2025-09-05 05:44:51 -0400

    TDX portal setup

---
+++ resources/js/script-client-portal.js
@@ -0,0 +1,6 @@
+document.head.insertAdjacentHTML(
+    'beforeend',
+    '<link rel="stylesheet" href="https://breakdancingcat.github.io/css/styles-client-portal.css" />',
+);
+
+document.body.classList.add('wsu-portal-page');
---
+++ resources/js/script-global.js
@@ -0,0 +1,19 @@
+document.head.insertAdjacentHTML(
+    'beforeend',
+    '<link rel="preconnect" href="https://fonts.googleapis.com">',
+);
+
+document.head.insertAdjacentHTML(
+    'beforeend',
+    '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>',
+);
+
+document.head.insertAdjacentHTML(
+    'beforeend',
+    '<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">'
+);
+
+document.head.insertAdjacentHTML(
+    'beforeend',
+    '<link rel="stylesheet" href="https://breakdancingcat.github.io/css/styles-global.css" />',
+);
