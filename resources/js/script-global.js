342225adbbb81bc5f71e3b27b2650ba891fc2883
Nick DeNardis <nick.denardis@gmail.com>
2026-03-12 11:02:27 -0400

    feat(Javascript): Google Tag Manager added to the global javascript

--- resources/js/script-global.js
+++ resources/js/script-global.js
@@ -17,3 +17,28 @@ document.head.insertAdjacentHTML(
     'beforeend',
     '<link rel="stylesheet" href="https://breakdancingcat.github.io/css/styles-global.css" />',
 );
+
+// After the DOM has fully loaded, inject Google Tag Manager
+document.addEventListener('DOMContentLoaded', function() {
+    const GTM_ID = 'GTM-N5M76JC';
+
+    // Inject <head>
+    const script = document.createElement('script');
+    script.innerHTML = `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+    `;
+    document.head.appendChild(script);  
+
+    // Inject <body> <noscript>
+    const noscript = document.createElement('noscript');
+    noscript.setAttribute('aria-hidden', 'true');
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://www.googletagmanager.com/ns.html?id=' + GTM_ID;
+    iframe.style.cssText = 'display:none;visibility:hidden;width:0;height:0';
+    noscript.appendChild(iframe);
+    document.body.insertBefore(noscript, document.body.firstChild);
+});
